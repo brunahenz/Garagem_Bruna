008929234eac52892b3936c6a2eec7af96c14e4a
brunahenz <brunaehenz@gmail.com>
2024-04-05 11:43:42 -0300

    criando as views

--- core/views/user.py
+++ core/views/user.py
@@ -3,7 +3,6 @@ from rest_framework.viewsets import ModelViewSet
 from core.models import User
 from core.serializers import UserSerializer
 
-
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
